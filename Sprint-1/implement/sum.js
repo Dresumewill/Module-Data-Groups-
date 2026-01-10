d8e2fbd736cce9723d70d2e2899b4ca62e57fc4b
Ifeyinwa <ifeyinwaofu@gmail.com>
2026-01-10 22:21:57 +0100

    if the input isn't an array return to 0

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,12 @@
 function sum(elements) {
+    if (!Array.isArray(elements)) return 0;
+
+    return elements.reduce((total, value) => {
+        if (typeof value === "number" && !isNaN(value)) {
+            return total + value;
+        }
+        return total;
+    }, 0);
 }
 
 module.exports = sum;
