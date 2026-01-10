4bf7213abb9abff4a8659f1e623ce8761c90e16f
Ifeyinwa <ifeyinwaofu@gmail.com>
2026-01-10 21:41:17 +0100

    this makes the function works with arrays

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,11 @@
 function findMax(elements) {
+    if (!Array.isArray(elements)) return null;
+
+    const numbers = elements.filter((value) => typeof value === "number" && !isNaN(value));
+    
+    if (numbers.length === 0) return null;
+
+    return Math.max(...numbers);
 }
 
 module.exports = findMax;
