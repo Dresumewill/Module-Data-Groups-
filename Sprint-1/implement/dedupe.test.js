804bb12624913674e9b38d9706156ef618e4e917
Ifeyinwa <ifeyinwaofu@gmail.com>
2026-01-19 15:55:54 +0100

    given the array with no duplicates, it returns the same value or array

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -17,11 +17,18 @@ E.g. dedupe([1, 2, 1]) target output: [1, 2]
 // When passed to the dedupe function
 // Then it should return an empty array
 // test.todo("given an empty array, it returns an empty array");
+describe("dedupe", () => {
+    it("given an empty array, it returns an empty array", () => {
+        expect(dedupe([])).toEqual([]);
+    });
+});
 
 // Given an array with no duplicates
 // When passed to the dedupe function
 // Then it should return a copy of the original array
-
+it("given an array with no duplicates, it returns the same value", () => { 
+    expect (dedupe([1, 2, 3, 4])).toEqual([1, 2, 3, 4]);
+});
 // Given an array with strings or numbers
 // When passed to the dedupe function
 // Then it should remove the duplicate values, preserving the first occurence of each element
