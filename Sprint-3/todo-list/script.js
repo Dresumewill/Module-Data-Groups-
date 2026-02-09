28d8cb3cc88c9bb1e6d703d84b20a8e09b071e1a
Ifeyinwa <ifeyinwaofu@gmail.com>
2026-02-09 09:28:12 +0100

    add complete toggle to todos

--- Sprint-3/todo-list/script.js
+++ Sprint-3/todo-list/script.js
@@ -11,3 +11,19 @@ function populateTodoList(todos) {
     // Task text
     const span = document.createElement("span");
     span.textContent = todo.task;
+
+    if (todo.completed) {
+      span.style.textDecoration = "line-through";
+    }
+
+    // Complete button
+    const completeBtn = document.createElement("button");
+    completeBtn.innerHTML = '<i class="fa fa-check"></i>';
+    completeBtn.className = "btn btn-success btn-sm";
+
+    completeBtn.addEventListener("click", () => {
+      span.style.textDecoration =
+        span.style.textDecoration === "line-through"
+          ? "none"
+          : "line-through";
+    });
